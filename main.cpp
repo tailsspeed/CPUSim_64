a94c58cffd4705a5f8f12cc67df5f5fba31595ad
TailsSpeed <kt.heart@gmail.com>
2024-11-21 15:00:12 -0800

    added exception handling for stoull

--- main.cpp
+++ main.cpp
@@ -31,15 +31,50 @@ int main(int argc, char const *argv[])
         if (line.length() != 16)
         {
             // cout << line << " is not a valid instruction" << endl;
-            printf("%s is not a valid instruction\n", line.c_str()); // .c_str() returns pointer to C-style string
+            printf("%s is too many hex digits\n", line.c_str()); // .c_str() returns pointer to C-style string
         }
         else
         {
-            uint64_t temp = stoull(line, nullptr, 16);
-            /* DECODE */
-            uint64_t opcode = temp & 0xFFF0000000000000; // upper 12 bits
-            uint64_t format = temp & 0xF000000000000000; // upper 4 bits
-            printf("0x%016lX\n     Opcode: 0x%lX\n     Format: 0x%lX\n", temp, opcode, format);
+            try
+            {
+                /*
+                stoull throws exception when argument is not in hex or exceeds range
+                    ex. terminate called after throwing an instance of 'std::invalid_argument'
+                        what():  stoull
+                 */
+                uint64_t temp = stoull(line, nullptr, 16);
+                /* DECODE */
+                uint64_t opcode = temp & 0xFFF0000000000000; // upper 12 bits
+                // uint64_t format = temp & 0xF000000000000000; // upper 4 bits
+                uint64_t format = temp >> 60; // upper 4 bits
+                printf("0x%016lX\n     Opcode: 0x%lX\n     Format: 0x%lX ", temp, opcode, format);
+                switch (format)
+                {
+                case 0xA:
+                    printf("Register->Register\n");
+                    break;
+                case 0xB:
+                    printf("Register->Memory\n");
+                    break;
+                case 0xC:
+                    printf("Memory->Register\n");
+                    break;
+                case 0xD:
+                    printf("Immediate->Register\n");
+                    break;
+                case 0xE:
+                    printf("Jump\n");
+                    break;
+                default:
+                    printf("Invalid format\n");
+                    break;
+                }
+            }
+            catch(const std::exception& e)
+            {
+                std::cerr << e.what() << '\n';
+                printf("%s could not be converted or exceeds 64-bit range\n", line.c_str());
+            }
         }
     }
     CPU ts;

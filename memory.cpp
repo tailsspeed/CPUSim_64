eb2e99042f92e5e8b1ac5563dd467f965d3fec4c
unknown <java.charmi@gmail.com>
2024-11-25 19:20:33 -0800

    memory defined with size..

--- memory.cpp
+++ memory.cpp
@@ -15,3 +15,32 @@ namespace m6502
 	struct CPU;
 	struct StatusFlags;
 }
+
+struct m6502::Mem
+{
+	static constexpr u32 MAX_MEM = 1024 * 64;
+	Byte Data[MAX_MEM];
+
+	void Initialise()
+	{
+		for ( u32 i = 0; i < MAX_MEM; i++ )
+		{
+			Data[i] = 0;
+		}
+	}
+
+	/** read 1 byte */
+	Byte operator[]( u32 Address ) const
+	{
+		// assert here Address is < MAX_MEM
+		return Data[Address];
+	}
+
+	/** write 1 byte */
+	Byte& operator[]( u32 Address )
+	{
+		// assert here Address is < MAX_MEM
+		return Data[Address];
+	}
+};
+

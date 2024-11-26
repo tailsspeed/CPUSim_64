0c314c30369b12dd47b9d36b646f5c707338326a
unknown <java.charmi@gmail.com>
2024-11-25 19:51:37 -0800

    reset function added

--- memory.cpp
+++ memory.cpp
@@ -2,11 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 namespace m6502
 {
 	using SByte = char;
 	using Byte = unsigned char;
 	using Word = unsigned short;
+
+    Word PC,SP;
 
 	using u32 = unsigned int;
 	using s32 = signed int;
@@ -54,4 +57,39 @@ struct m6502::StatusFlags
 	Byte Unused : 1; //Unused
 	Byte V : 1; //Overflow
 	Byte N : 1; //Negative
+
+  
 };
+
+struct CPU{
+    using Byte = unsigned char;
+    using Word = unsigned char;
+
+    Word SP; //stack pntr
+    Word PC; //prgm counter
+    Byte A,X,Y; //registers
+
+    Byte C : 1;	// Carry Flag	
+	Byte Z : 1;	//Zero Flag
+	Byte I : 1; //Interrupt disable
+	Byte D : 1; //Decimal mode
+	Byte B : 1; //Break
+	Byte Unused : 1; //Unused
+	Byte V : 1; //Overflow
+	Byte N : 1; //Negative
+
+  void Reset(){
+        PC = 0xFFFC;
+        C = Z = I = D=0;
+        SP = 0X0100;
+        A=X=Y=0;
+        
+    }
+};
+
+int main(){
+    CPU cpu;
+    cpu.Reset();
+
+    return 0;
+}
